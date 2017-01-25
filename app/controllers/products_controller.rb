c73108f04bbbbce1f55b06320fb59ea5565f0468
Priyanka <priyanka3007@gmail.com>
2017-01-24 17:27:38 -0800

    error chk

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,18 @@
 class ProductsController < ApplicationController
   def index
   	@products =Product.all
-  	render json:@products
+  	# render json:@products
   end
   def create
-  @product = Product.create( name: params[:name], description: params[:description])
-  puts "jkbhjbhgvhgvghvg", @product, "lknsjkdbjhwbd"
-  # render :text =>@product.inspect
-  redirect_to '/products/index'
+  @product = Product.create( product_params )
+  redirect_to '/products'
 end
+def update
+  @product = Product.update( product_params )
+  redirect_to '/products'
+end
+private 
+  def product_params
+   params.require(:product).permit(:name, :description)
+  end
 end
